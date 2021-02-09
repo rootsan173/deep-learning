28f623a0aa2a1d7cefc7105c31542aeee8b45295
rootsan173 <rootsan.173@gmail.com>
2021-02-09 17:09:39 +0900

    add other gate

--- chapter-2/logic-circuit.py
+++ chapter-2/logic-circuit.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 #ANDゲート
 def AND(x1, x2):
   w1, w2, theta = 0.5, 0.5, 0.7
@@ -6,3 +8,43 @@ def AND(x1, x2):
     return 0
   elif tmp > theta:
     return 1
+
+#ANDゲート（バイアスに変形）
+def AND2(x1, x2):
+  x = np.array([x1, x2])
+  w = np.array([0.5, 0.5])
+  b = -0.7
+  tmp = np.sum(w + x) + b
+  if tmp <= 0:
+    return 0
+  else:
+    return 1
+
+#NANDゲート
+def NAND(x1, x2):
+  x = np.array([x1, x2])
+  w = np.array([-0.5, -0.5])
+  b = 0.7
+  tmp = np.sum(w + x) + b
+  if tmp <= 0:
+    return 0
+  else:
+    return 1
+
+#ORゲート
+def OR(x1, x2):
+  x = np.array([x1, x2])
+  w = np.array([0.5, 0.5])
+  b = -0.2
+  tmp = np.sum(w + x) + b
+  if tmp <= 0:
+    return 0
+  else:
+    return 1
+
+#XORゲート
+def XOR(x1, x2):
+  s1 = NAND(x1, x2)
+  s2 = OR(x1, x2)
+  y = AND(s1, s2)
+  return y
